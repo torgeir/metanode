da4105bc6777d261bccf58f83a8cb6509b9ec857
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2010-06-27 12:44:34 +0200

    added menu, removed metadatajs

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-var t = require('./vendor/tnode/t');
-var metadata = require('./vendor/metadata');
+require.paths.unshift(__dirname + '/actions/');
+require.paths.unshift(__dirname + '/vendor/');
+
+var sys = require('sys');
+var t = require('tnode/t');
+var search = require('search/search');
 
 t.app({
+    debug: true,
     routes: {
         '^/(web/.*)$': t.serve,
+        '^/search/?$': search.index,
+        '^/search/(.*)/?$': search.search,
         '^/$': function(req, res) {
             return 'index';
         }
